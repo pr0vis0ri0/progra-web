6ae726353850834a3c258bd01d68d87a25876c4b
RemiCajayena <es.ruminot@duocuc.cl>
2023-06-17 10:08:09 -0400

    commit

--- Pagina/Appventas/static/js/lindito.js
+++ Pagina/Appventas/static/js/lindito.js
@@ -22,15 +22,15 @@ function checkFormulario () {
             var email = $('#email').val();
             var emailLength = email.length;
             if (email != "" && emailLength > 4) {
-                email = email.replace(/\s/g, "");  //To remove space if available
+                email = email.replace(/\s/g, "");  
                 var atIndex = email.indexOf("@");
 
-                if (atIndex > -1) {   //To check (.) and (@) present in the string
-                    if ( atIndex != 0 && atIndex != emailLength && email.slice(email.length - 1) != ".") {   //To check (.) and (@) are not the firat or last character of string
+                if (atIndex > -1) {  
+                    if ( atIndex != 0 && atIndex != emailLength && email.slice(email.length - 1) != ".") {   
                         
                         var atCount = email.split('@').length
 
-                        if (atCount == 2 && (atIndex > 1)) { //To check (@) present multiple times or not in the string. And (.) and (@) not located subsequently
+                        if (atCount == 2 && (atIndex > 1)) { 
                             status = true;
                            
                         }
